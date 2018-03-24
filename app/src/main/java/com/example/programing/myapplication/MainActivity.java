4be3c46795324755503d36080bc28f4f9670600a
hemmy6894 <37750218+hemmy6894@users.noreply.github.com>
2018-03-24 23:46:01 +0300

    Something added

--- app/src/main/java/com/example/programing/myapplication/MainActivity.java
+++ app/src/main/java/com/example/programing/myapplication/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //This sms updated my my update.
+
+        //commmiting suited
     }
 }
